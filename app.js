c1a4be3b7fe72c6f72e203efb095d770976f49d3
Glitch (zewaka-keywordlabeler) <none>
2020-07-02 23:52:41 +0000

    Better formatting

--- app.js
+++ app.js
@@ -24,29 +24,28 @@ module.exports = robot => {
       let labelsToAdd = [];
 
       var ourIssueOrPR = context.payload.issue;
-      if (ourIssueOrPR == null) ourIssueOrPR = context.payload.pull_request; //If there's no issue field, then it's a pull request trigger 😎
+			if (ourIssueOrPR == null) ourIssueOrPR = context.payload.pull_request; //If there's no issue field, then it's a pull request trigger 😎
 
-      let fetchedTitle = ourIssueOrPR.title;
-      let fetchedBody = ourIssueOrPR.body;
+			let fetchedTitle = ourIssueOrPR.title;
+			let fetchedBody = ourIssueOrPR.body;
 
-      for (let token in config.labelMappings) {
-        if (config.caseSensitive === false) {
-          token = token.toLowerCase();
-          fetchedTitle = fetchedTitle.toLowerCase();
-          fetchedBody = fetchedBody.toLowerCase();
-        }
+			for (let token in config.labelMappings)
+			{
 
-        if (
-          (config.matchTitle ? fetchedTitle.includes(token) : false) ||
-          (config.matchBody ? fetchedBody.includes(token) : false)
-        ) {
+				if (config.caseSensitive === false)
+				{
+					token = token.toLowerCase()
+					fetchedTitle = fetchedTitle.toLowerCase()
+					fetchedBody = fetchedBody.toLowerCase()
+				}
+
+        if ((config.matchTitle ? fetchedTitle.includes(token) : false) || (config.matchBody ? fetchedBody.includes(token) : false))
+				{
           labelsToAdd.push(config.labelMappings[token]);
         }
       }
 
-      return context.github.issues.addLabels(
-        context.issue({ labels: labelsToAdd })
-      );
+      return context.github.issues.addLabels(context.issue({ labels: labelsToAdd }));
     }
   );
 };
